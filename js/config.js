93473fc060548d85111d99eba88beeaa1de71a9a
Ian2003227 <mynameisian2003227@gmail.com>
2026-08-17 17:11:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Fill GAS_WEB_APP_URL in after deploying gas/Code.gs as a Web App (see gas/README.md).
 window.RorConfig = {
-  GAS_WEB_APP_URL: "",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwUYJFNIupxcd2W74doEw4wbIvLIOikP-kmMnkC3WKyjX5BdP5ihPJqk5xYjpwAgkt7/exec",
   USERS: ["郭", "彥", "慧", "言"],
 };
